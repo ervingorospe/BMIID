00daafadc604aaa8fa17413197b1fbf132541acb
ervin <ervingorospe123@gmail.com>
2021-07-12 20:14:35 +0800

    sanctum domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        'localhost,127.0.0.1'
+        'localhost,127.0.0.1,bmiid-4r7l7,bmiid-4r7l7.ondigitalocean.app'
     )),
 
     /*
